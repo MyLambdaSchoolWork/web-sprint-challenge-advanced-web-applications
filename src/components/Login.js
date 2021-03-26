df404a616954900ea73ab75120927f7b9b87d3de
Matthew Ellis <mbellistx@gmail.com>
2021-03-26 13:53:15 -0500

    login form html/css done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+
+import eyeVisible from '../assets/iconmonstr-eye-thin.svg';
+import eyeNotVisible from '../assets/iconmonstr-eye-off-thin.svg';
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+const initialFocus = {
+  username: false,
+  password: false,
+};
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const [ values, setValues ] = useState(initialValues)
+  const [ error, setError ] = useState('')
+  const [ focus, setFocus ] = useState(initialFocus)
+  const [ passwordVisible, setPasswordVisible ] = useState(false)
 
-  useEffect(()=>{
-    // make a post request to retrieve a token from the api
-    // when you have handled the token, navigate to the BubblePage route
-  });
+  function onChange(evt) {
+    const { name, value } = evt.target
+    setValues({ ...values, [name]: value })
+  }
+
+  function onFocus(evt) {
+    const { name } = evt.target
+    setFocus({ ...focus, [name]: true })
+  }
   
-  const error = "";
+  function onBlur(evt) {
+    const { name } = evt.target
+    const value = values[name] === '' ? false : true
+    setFocus({...focus, [name]: value});
+  }
+  
   //replace with error state
 
   return (
@@ -17,6 +43,48 @@ const Login = () => {
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
         <h2>Build login form here</h2>
+        <form>
+          <DivFieldsetStyled>
+            <LabelStyled 
+              focus={focus.username}
+              htmlFor='username'
+              // hasData={values.username === '' ? false : true}
+            >
+              Username
+            </LabelStyled>
+            <InputStyled 
+              id='username'
+              type='text'
+              name='username'
+              value={values.username}
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}
+            />
+          </DivFieldsetStyled>
+          <DivFieldsetStyled>
+            <LabelStyled 
+              focus={focus.password}
+              htmlFor='password'
+            >
+              Password
+            </LabelStyled>
+            <InputStyled 
+              id='password'
+              type={passwordVisible ? 'text' : 'password'}
+              name='password'
+              value={values.password}
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}
+            />
+            <ImgEyeStyled 
+              src={passwordVisible ? eyeNotVisible : eyeVisible}
+              alt=''
+              onClick={() => setPasswordVisible(!passwordVisible)}
+            />
+          </DivFieldsetStyled>
+        </form>
       </div>
 
       <p data-testid="errorMessage" className="error">{error}</p>
@@ -26,6 +94,51 @@ const Login = () => {
 
 export default Login;
 
+// Thank you Chris from css tricks for giving me the css to make the "fieldset" work, ready to copy and paste
+// https://css-tricks.com/snippets/css/non-form-fieldset-look/
+const DivFieldsetStyled = styled.div`
+  border: 1px solid black;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin: 10px auto;
+  padding: 10px;
+  position: relative;
+  text-align: left;
+  width: 300px;
+`
+
+const LabelStyled = styled.label`
+  background: #fff;
+  color: ${props => {
+    return props.focus ? 'black' : 'grey'
+  }};
+  font-size: ${props => {
+    return props.focus ? '18px' : '24px'
+  }};
+  line-height: 1;
+  margin-top: ${props => {
+    return props.focus ? '-9px' : '8px'
+  }}; /* negative margin half of fontsize, other margin trial and error ig */
+  padding: 0 3px;
+  position: absolute;
+  top: 0;
+  transition: margin-top 0.15s, font-size 0.15s, color 0.15s; // transitions from over input to part of div
+`
+
+const InputStyled = styled.input`
+  border: none;
+  display: inline-block;
+  font-size: 20px;
+  outline: none;
+  width: 250px;
+`
+
+const ImgEyeStyled = styled.img`
+  display: inline-block;
+  height: 16px;
+  margin-top: 4px;
+  width: 20px;
+`
 //Task List:
 //1. Build a form containing a username and password field.
 //2. Add whatever state nessiary for form functioning.
